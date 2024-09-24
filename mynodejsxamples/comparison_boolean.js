016548a028464a8ea9980927408dff825eda2ed9
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2024-09-24 23:05:01 +0530

    Commit changes...

--- mynodejsxamples/comparison_boolean.js
+++ mynodejsxamples/comparison_boolean.js
@@ -1,6 +1,7 @@
 const bool = true;
+
 const num = 1;
 
-console.log('bool == num) : ', (bool == num)); // true - The boolean value true gets converted to 1
+console.log('(bool == num) : ', (bool == num)); // true - The boolean value true gets converted to 1
 
-console.log('bool === num) : ', (bool === num)); // false - The values are different types and not equal
+console.log('(bool === num) : ', (bool === num)); // false - The values are different types and not equal
