50aa1371acdb679e1fbf570529b93873a7bcde47
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2024-11-30 13:31:14 +0530

    Commit changes...

--- mynodejsxamples/array_groupBy.js
+++ mynodejsxamples/array_groupBy.js
@@ -1,13 +1,9 @@
-
 const people = [
-    { name: 'Alice', age: 21 },
-    { name: 'Bob', age: 22 },
-    { name: 'Charlie', age: 21 }
-  ];
-  
- // const groupedByAge = people.groupBy(person => person.age);
-  //console.log(groupedByAge);
+  { name: 'Alice', age: 21 },
+  { name: 'Bob', age: 22 },
+  { name: 'Charlie', age: 21 }
+];
 
-  const peopleByAge = Object.groupBy(people, (person) => person.age);
-  console.log(groupedByAge);
-  // Output: { '21': [{ name: 'Alice', age: 21 }, { name: 'Charlie', age: 21 }], '22': [{ name: 'Bob', age: 22 }] }
+const groupedByAge = people.groupBy(person => person.age);
+
+console.log(groupedByAge);
