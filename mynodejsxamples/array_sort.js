a2efe67cdc84a27d29f5b63c21028d824262bafd
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2024-11-30 10:23:41 +0530

    committing node_modules

---
+++ mynodejsxamples/array_sort.js
@@ -0,0 +1,13 @@
+const fruits = ["orange", "apple", "banana"];
+
+fruits.sort();
+
+console.log ('fruits = ', fruits);
+
+//Reverse sort
+const fruits1 = ["Banana", "Orange", "Apple", "Mango"];
+
+fruits1.reverse();
+
+console.log ('fruits1 = ', fruits1);
+//Reverse sort
---
+++ mynodejsxamples/array_toSorted.js
@@ -0,0 +1,7 @@
+const months = ["Jan", "Feb", "Mar", "Apr"];
+
+const sortedMonths = months.toSorted();
+
+console.log ('months = ', months);
+
+console.log ('sortedMonths = ', sortedMonths);
