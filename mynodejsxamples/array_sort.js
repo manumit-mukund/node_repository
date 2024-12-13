6d41241a4d0e74f704123959aa304d1bc9f3e3f9
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2024-12-13 23:42:47 +0530

    Commit changes...

--- mynodejsxamples/array_sort.js
+++ mynodejsxamples/array_sort.js
@@ -4,7 +4,7 @@ console.log ('fruits = ', fruits);
 
 fruits.sort();
 
-console.log ('fruits = ', fruits);
+console.log ('fruits sorted = ', fruits);
 
 //**********************Reverse sort*************************
 const fruits1 = ["Banana", "Orange", "Apple", "Mango"];
@@ -15,5 +15,5 @@ fruits1.sort();
 
 fruits1.reverse();
 
-console.log ('fruits1 = ', fruits1);
+console.log ('fruits1 reverse sorted = ', fruits1);
 //**********************Reverse sort*************************
