8145e3a9f69b6ed6da3e7ad219cb227e28cd5aa0
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2025-01-02 16:59:44 +0530

    Commit changes...

--- mynodejsxamples/array_numericSort.js
+++ mynodejsxamples/array_numericSort.js
@@ -1,15 +1,17 @@
 const points = [40, 100, 1, 5, 25, 10];
 
+console.log('points = ', points);
+
 points.sort(function (a, b) {
     return a - b
 }
 );
 
-console.log('points = ', points);
+console.log('sorted points = ', points);
 
 points.sort(function (a, b) {
     return b - a
 }
 );
 
-console.log('points = ', points);
+console.log('reverse sorted points = ', points);
