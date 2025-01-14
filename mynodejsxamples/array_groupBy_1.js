25038aad1b71cb11c923cd5197349d5d22fd4a94
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2025-01-14 14:17:45 +0530

    Commit a new file...

---
+++ mynodejsxamples/array_groupBy_1.js
@@ -0,0 +1,20 @@
+require("core-js/actual/array/group-by-to-map");
+require("core-js/actual/array/group-by");
+
+
+const people = [
+
+  { name: 'N1', age: 21 },
+  { name: 'N2', age: 22 },
+  { name: 'N3', age: 22 },
+  { name: 'N4', age: 21 },
+  { name: 'N5', age: 25, city: "Delhi" },
+ 
+  
+];
+
+const groupedByAge = people.groupBy(person => person.age);
+
+console.log(groupedByAge);
+
+console.table(groupedByAge);
