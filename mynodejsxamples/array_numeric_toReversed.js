2e2ec8728cf7eccb02a0e6d29c8622e485c3a9f7
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2025-01-16 22:41:30 +0530

    Commit changes...

--- mynodejsxamples/array_numeric_toReversed.js
+++ mynodejsxamples/array_numeric_toReversed.js
@@ -5,10 +5,3 @@ console.log('values = ', values);
 const sortedValues = values.toReversed();
 
 console.log('sortedValues = ', sortedValues);
-
-const fruits = ["Banana", "Orange", "Apple", "Mango"];
-
-const fruitsReversed = fruits.toReversed();
-
-console.log('fruitsReversed = ', fruitsReversed);
-
