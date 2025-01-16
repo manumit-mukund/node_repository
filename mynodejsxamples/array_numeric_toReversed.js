3a013a112f853f11642d988f8960eaabbf483d2c
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2025-01-16 22:38:15 +0530

    Commit a new file...

---
+++ mynodejsxamples/array_numeric_toReversed.js
@@ -0,0 +1,14 @@
+const values = [5, 1, 3, 2, 4];
+
+console.log('values = ', values);
+
+const sortedValues = values.toReversed();
+
+console.log('sortedValues = ', sortedValues);
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+const fruitsReversed = fruits.toReversed();
+
+console.log('fruitsReversed = ', fruitsReversed);
+
