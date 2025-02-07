fd0a1755e2a0652b2a8b9efeaec08e8f1e848bd9
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2025-02-07 21:32:15 +0530

    Commit changes...

--- mynodejsxamples/spread_operator_concatenate_arrays.js
+++ mynodejsxamples/spread_operator_concatenate_arrays.js
@@ -8,6 +8,15 @@ const arr12 = [...arr1, ...arr2];
 
 const arr23 = [...arr2, ...arr3];
 
+const arr13 = [...arr1, ...arr3];
+
+const arr123 = [...arr1, ...arr2, ...arr3];
+
+
 console.log('arr12 = ', arr12); 
 
 console.log('arr23 = ', arr23); 
+
+console.log('arr13 = ', arr13); 
+
+console.log('arr123 = ', arr123); 
