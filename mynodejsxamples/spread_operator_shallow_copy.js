095da3c71b82ce5e0cccabd67d1061c4e62abf56
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2025-02-17 13:38:30 +0530

    Commit changes...

--- mynodejsxamples/spread_operator_shallow_copy.js
+++ mynodejsxamples/spread_operator_shallow_copy.js
@@ -2,8 +2,26 @@ const originalArray = [[1, 2, 3, 4], 12];
 
 const copiedArray = [...originalArray];
 
-copiedArray[0].push(99);
-
 console.log('originalArray ', originalArray);
 
 console.log('copiedArray = ', copiedArray);
+
+copiedArray[0].push(99);
+
+console.log('\noriginalArray ', originalArray);
+
+console.log('copiedArray = ', copiedArray);
+
+copiedArray.push(99);
+
+console.log('\noriginalArray ', originalArray);
+
+console.log('copiedArray = ', copiedArray);
+
+//copiedArray[0][0].push(99); // TypeError: copiedArray[0][0].push is not a function
+
+console.log('\ncopiedArray[0][0] ', copiedArray[0][0]);
+
+console.log('copiedArray[0][1] ', copiedArray[0][1]);
+
+//console.log('copiedArray = ', copiedArray);
