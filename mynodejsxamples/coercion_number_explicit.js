d1453474e707104633e8d6a7329699a24f49668a
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2025-03-26 17:33:25 +0530

    Commit changes...

--- mynodejsxamples/coercion_number_explicit.js
+++ mynodejsxamples/coercion_number_explicit.js
@@ -3,3 +3,11 @@ console.log(Number("123"));
 console.log();
 
 console.log(Number("1a3"));
+
+console.log();
+
+console.log(parseInt("42px"));
+
+console.log();
+
+console.log(parseFloat("3.14abc")); 
