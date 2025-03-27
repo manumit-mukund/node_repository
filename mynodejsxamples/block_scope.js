a7f205e854743dd5892ea53f0ed5cbdb688e6366
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2025-03-27 23:26:21 +0530

    Commit changes...

--- mynodejsxamples/block_scope.js
+++ mynodejsxamples/block_scope.js
@@ -8,10 +8,10 @@
     var z = 4;
     console.log('\nz = ' + z);
 
-  
 }
 
 // console.log('x = ' + x); ReferenceError: x is not defined
-//console.log('\ny = ' + y); //ReferenceError: y is not defined
+// console.log('\ny = ' + y); //ReferenceError: y is not defined
+
 console.log('\nz = ' + z);
 
