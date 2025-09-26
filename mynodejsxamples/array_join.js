161ae02b97a2be2196ea68ea12e109405a17019a
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2025-09-26 11:31:14 +0530

    Commit changes...

--- mynodejsxamples/array_join.js
+++ mynodejsxamples/array_join.js
@@ -2,8 +2,12 @@ const fruits = ["apple", "banana", "orange"];
 
 console.log ('fruits = ', fruits);
 
-const result = fruits.join();
+const result1 = fruits.join();
+
+const result2 = fruits.join(" ");
 
 console.log ('\nfruits after join = ', fruits);
 
-console.log ('\nresult = ', result);
+console.log ('\nresult1 = ', result1);
+
+console.log ('\nresult2 = ', result2);
