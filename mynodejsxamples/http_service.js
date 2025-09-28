e640773d64bf79332128083f830579e5223581e6
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2025-09-28 17:16:03 +0530

    Commit changes...

--- mynodejsxamples/http_service.js
+++ mynodejsxamples/http_service.js
@@ -1,12 +1,14 @@
 var http = require("http");
 
-http.createServer(function (request, response) {
+http
+   .createServer(function (request, response) {
 
-   response.writeHead(200, { 'Content-Type': 'text/plain' });
+      response.writeHead(200, { 'Content-Type': 'text/plain' });
 
-   // Send the response text as "Hello World"
-   response.end('Hello World\n');
-   
-}).listen(3100);
+      // Send the response text as "Hello World"
+      response.end('Hello World\n');
+
+   })
+   .listen(3100);
 
 console.log('Server running at http://127.0.0.1:3100/');
