b8df61bfad85fe6e0d24840a17106986830477bc
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2026-01-12 12:45:23 +0530

    Commit changes...

---
+++ mynodejsxamples/array_string_sort_ascending.js
@@ -0,0 +1,11 @@
+const fruits = ["orange", "apple", "banana"];
+
+console.log('fruits = ', fruits);
+
+fruits.sort();
+
+console.log();
+
+console.log('fruits sorted ascending = ', fruits);
+
+console.log();
---
+++ mynodejsxamples/array_string_sort_descending.js
@@ -0,0 +1,12 @@
+const fruits = ["banana", "orange", "apple", "mango"];
+
+console.log('fruits = ', fruits);
+
+fruits.sort();
+
+fruits.reverse();
+
+console.log();
+
+console.log('fruits1 sorted descending = ', fruits);
+//**********************Reverse sort*************************
