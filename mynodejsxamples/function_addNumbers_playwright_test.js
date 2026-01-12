3f1635dd1d17122b8aa9eef94327a4b577024d34
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2026-01-12 22:23:31 +0530

    Commit changes...

--- mynodejsxamples/function_addNumbers_playwright_test.js
+++ mynodejsxamples/function_addNumbers_playwright_test.js
@@ -1,21 +1,27 @@
 // addition.spec.js
 const { test, expect } = require('@playwright/test');
-const { addNumbers } = require('function_addNumbers'); // Import the add function
+const { addNumbers } = require('addNumbers'); // Import the add function
 
 test('should correctly add two numbers', async ({ page }) => {
+ 
   // Call the add function directly in your test
   const result = addNumbers(5, 3);
 
   // Assert that the result is as expected
   await expect(result).toBe(8);
+
 });
 
 test('should handle negative numbers correctly', async ({ page }) => {
+
   const result = addNumbers(-5, 3);
   await expect(result).toBe(-2);
+
 });
 
 test('should handle zero correctly', async ({ page }) => {
+
   const result = addNumbers(10, 0);
   await expect(result).toBe(10);
+
 });
