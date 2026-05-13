93d0f6abba4adf0b517a89708843f022348f89f1
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2026-05-13 15:49:00 +0530

    Commit changes...

--- mynodejsxamples/array_concat.js
+++ mynodejsxamples/array_concat.js
@@ -3,5 +3,8 @@ const originalFruits = ["apple", "banana"];
 console.log('originalFruits : ', originalFruits);
 
 const newFruits = originalFruits.concat("orange");
+
+console.log('\noriginalFruits : ', originalFruits);
+
 console.log('\nnewFruits : ', newFruits);
 
--- mynodejsxamples/array_push.js
+++ mynodejsxamples/array_push.js
@@ -3,8 +3,10 @@ const fruits = ["apple", "banana"];
 console.log('fruits : ', fruits);
 
 fruits.push("orange");
+
 console.log('\nfruits : ', fruits);
 
 fruits.push("grape", "kiwi");
+
 console.log('\nfruits : ', fruits);
 
