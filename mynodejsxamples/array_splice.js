bbc50f1d742d426476174bb0a6f73400dd0f90d9
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2026-05-13 15:56:15 +0530

    Commit changes,,,

--- mynodejsxamples/array_splice.js
+++ mynodejsxamples/array_splice.js
@@ -1,7 +1,11 @@
-const fruits = ["apple", "mango", "grape"];
+const fruits = ["apple", "mango", "grapes"];
 
 console.log('fruits : ', fruits);
 
 fruits.splice(1, 0, "banana");
+
 console.log('\nfruits : ', fruits);
 
+fruits.splice(1, 1, "kiwi");
+
+console.log('\nfruits : ', fruits);
